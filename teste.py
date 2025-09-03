2a0c439ba4b74999dc455e5c6c46a26cf15684f8
Felipe Correia Costa <felipe.correia@gkt.grupokabum.com.br>
2025-09-03 13:25:18 -0300

    Implementação do gitignore

--- teste.py
+++ teste.py
@@ -1,1 +1,11 @@
-print(help)
+class pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        
+    def exibir_info(self):
+        print(f"nome: {self.nome}, idade: {self.idade}")
+        
+
+pessoa1 = pessoa('Felipe',18)
+pessoa1.exibir_info()
